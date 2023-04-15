0629f9c19028abe37e60132332d04eeb9c45d842
ImKetan1610 <ketan.makode@gmail.com>
2023-04-15 23:49:28 +0530

    updated db

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
   try {
-    const connect = mongoose.connect(process.env.URL);
+    const connect = mongoose.connect(
+      "mongodb+srv://Ketan:Ketan1610@cluster0.rwvk47z.mongodb.net/?retryWrites=true&w=majority"
+    );
   } catch (error) {
     console.log(error);
     throw new Error(error);
